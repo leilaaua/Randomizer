f6dc00303fdd0091ad794a59f03460ca8471e45e
leilaaua <leila0777@icloud.com>
2021-09-19 00:59:14 +0400

    initial commit

--- Randomizer/ViewControllers/MainViewController.swift
+++ Randomizer/ViewControllers/MainViewController.swift
@@ -33,5 +33,11 @@ class MainViewController: UIViewController {
         resultLabel.text = String(Int.random(in: minimumNumber...maximumNumber))
     }
     
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let settingsVC = segue.source as? SettingsViewController else { return }
+        minimumValueLabel.text = settingsVC.minimumValueTF.text
+        maximumValueLabel.text = settingsVC.maximumValueTF.text
+    }
+    
 }
 
